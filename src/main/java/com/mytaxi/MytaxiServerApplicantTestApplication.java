e3c6395aa8896f2e1f45da90d674ee9d33ea416a
Gustavo Andrade <gustavoandrade82@gmail.com>
2018-08-19 11:06:01 -0300

    Adding API security layer

--- src/main/java/com/mytaxi/MytaxiServerApplicantTestApplication.java
+++ src/main/java/com/mytaxi/MytaxiServerApplicantTestApplication.java
@@ -1,21 +1,34 @@
 package com.mytaxi;
 
 import com.mytaxi.util.LoggingInterceptor;
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.BasicAuth;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class MytaxiServerApplicantTestApplication extends WebMvcConfigurerAdapter
+@EnableWebSecurity
+public class MytaxiServerApplicantTestApplication extends WebSecurityConfigurerAdapter implements WebMvcConfigurer
 {
 
     public static void main(String[] args)
@@ -39,7 +52,7 @@ public class MytaxiServerApplicantTestApplication extends WebMvcConfigurerAdapte
             .apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
             .paths(PathSelectors.any())
             .build()
-            .apiInfo(generateApiInfo());
+            .apiInfo(generateApiInfo()).securitySchemes(Arrays.asList(new BasicAuth("basicAuth")));
     }
 
 
@@ -47,5 +60,36 @@ public class MytaxiServerApplicantTestApplication extends WebMvcConfigurerAdapte
     {
         return new ApiInfo("mytaxi Server Applicant Test Service", "This service is to check the technology knowledge of a server applicant for mytaxi.", "Version 1.0 - mw",
             "urn:tos", "career@mytaxi.com", "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        manager.createUser(User.withUsername("mytaxi").password(encoder.encode("mytaxi")).roles("ADMIN").build());
+        return manager;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/v1/**").authenticated()
+            .and()
+                .httpBasic().and().formLogin();
+
+        allowH2Console(http);
+    }
+
+
+    private void allowH2Console(HttpSecurity http) throws Exception
+    {
+        http.csrf()
+            .disable()
+            .headers()
+            .frameOptions().disable();
     }
 }
