f7cc1aa224745a2143c295c217775f144da5e576
Gustavo Andrade <gustavoandrade82@gmail.com>
2018-08-21 13:08:37 -0300

    Changing exception message & status code

--- src/main/java/com/mytaxi/exception/CarAlreadyInUseException.java
+++ src/main/java/com/mytaxi/exception/CarAlreadyInUseException.java
@@ -3,7 +3,7 @@ package com.mytaxi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Entity already in use")
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Car already in use")
 public class CarAlreadyInUseException extends Exception
 {
     public CarAlreadyInUseException(String message)
