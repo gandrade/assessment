e19226ec768999b1ec19c29ce342b9251d6f25ed
Gustavo Andrade <gustavoandrade82@gmail.com>
2018-12-24 10:00:46 -0200

    Polishing javadocs

--- src/main/java/com/mytaxi/dataaccessobject/CarRepository.java
+++ src/main/java/com/mytaxi/dataaccessobject/CarRepository.java
@@ -4,6 +4,10 @@ import com.mytaxi.domainobject.CarDO;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Database Access Object for car table.
+ * <p/>
+ */
 public interface CarRepository extends CrudRepository<CarDO, Long>
 {
     Optional<CarDO> findByIdAndDriverDOIsNull(Long carId);
--- src/main/java/com/mytaxi/dataaccessobject/ManufacturerRepository.java
+++ src/main/java/com/mytaxi/dataaccessobject/ManufacturerRepository.java
@@ -4,6 +4,10 @@ import com.mytaxi.domainobject.ManufacturerDO;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Database Access Object for manufacturer table.
+ * <p/>
+ */
 public interface ManufacturerRepository extends CrudRepository<ManufacturerDO, Long>
 {
 
