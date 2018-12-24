7cbe9aec8f5769b8b9cf2f19c0fd7e021b7a17fe
Gustavo Andrade <gustavoandrade82@gmail.com>
2018-12-24 09:38:40 -0200

    Resolving deprecated Swagger code after bump it to the newest version

--- src/main/java/com/mytaxi/core/SwaggerConfiguration.java
+++ src/main/java/com/mytaxi/core/SwaggerConfiguration.java
@@ -7,11 +7,13 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -24,12 +26,19 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage(MytaxiServerApplicantTestApplication.class.getPackage().getName()))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(generateApiInfo()).securitySchemes(Arrays.asList(new BasicAuth("basicAuth")));
+                .apiInfo(generateApiInfo())
+                .securitySchemes(Arrays.asList(new BasicAuth("basicAuth")));
     }
 
 
     private ApiInfo generateApiInfo() {
-        return new ApiInfo("mytaxi Server Applicant Test Service", "This service is to check the technology knowledge of a server applicant for mytaxi.", "Version 1.0 - mw",
-                "urn:tos", "career@mytaxi.com", "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+        return new ApiInfo("mytaxi Server Applicant Test Service",
+                "This service is to check the technology knowledge of a server applicant for mytaxi.",
+                "Version 1.0 - mw",
+                "urn:tos",
+                new Contact(null, null, "career@mytaxi.com"),
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList());
     }
 }
