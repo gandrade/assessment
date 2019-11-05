d21e03765ddfefceffbb62f43e427d6504cf3223
Gustavo Andrade <gustavoandrade82@gmail.com>
2019-11-04 22:49:49 -0300

    Removing logging instruction from exceptions

--- src/main/java/com/mytaxi/exception/EntityNotFoundException.java
+++ src/main/java/com/mytaxi/exception/EntityNotFoundException.java
@@ -1,6 +1,5 @@
 package com.mytaxi.exception;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -9,13 +8,9 @@ public class EntityNotFoundException extends Exception
 {
     static final long serialVersionUID = -3387516993334229948L;
 
-    private static org.slf4j.Logger LOG = LoggerFactory.getLogger(EntityNotFoundException.class);
-
 
     public EntityNotFoundException(String message)
     {
         super(message);
-        LOG.warn(message);
     }
-
 }
